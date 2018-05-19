72b180a2620e19da4bd6a9dbad3d81a4d92f89ac
Sharmina Akhtar <sakht318@gmail.com>
2018-05-19 01:49:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Write your code in this file!
 const currentUser = 'Mina';
-const welcomeMessage = 'Welcome to Flatbook, ' + currentUser '!';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+ 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
